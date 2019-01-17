76519ca4814b7e0a648477dd5a616683c9f4f5dd
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-17 13:47:54 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,5 +10,6 @@ class Backer
     def back_project(project)
         @backed_projects << project
         project.add_backer(self)
+    end
 
 end
--- lib/project.rb
+++ lib/project.rb
@@ -9,6 +9,7 @@ class Project
 
     def add_backer(backer)
         @backers << backer
+        backer.back_project(self)
     end
 
 end
